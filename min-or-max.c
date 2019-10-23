4e784b6d1abbf098f8846c0cc262e968cef3ada9
Darya Pakhomova <56807101+mrrlanie@users.noreply.github.com>
2019-10-23 10:32:06 +1000

    Add files via upload

---
+++ min-or-max.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+ int main() {
+    int N;
+    scanf("%i", &N);
+    int A[N];
+    scanf("%i", &A[0]);
+    int max = A[0];
+    for (int i = 0; i < N-1; ++i) {
+        scanf("%i", &A[i]);
+        if (A[i] > max){
+            max = A[i];
+        }
+    }
+    printf("%i\n", max);
+ }
