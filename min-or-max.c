cac2dd61efb99a9584d9ad3f96c501e83ea21636
Darya Pakhomova <56807101+mrrlanie@users.noreply.github.com>
2019-10-29 12:36:11 +1000

    Update min-or-max.c

--- min-or-max.c
+++ min-or-max.c
@@ -3,8 +3,10 @@
  int main() {
     int N;
     scanf("%i", &N);
+  
     int A[N];
     scanf("%i", &A[0]);
+  
     int max = A[0];
     for (int i = 0; i < N-1; ++i) {
         scanf("%i", &A[i]);
